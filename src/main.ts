05f8a4a835f0ec0cee1f10f145c2ac9f3f741f23
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-12-28 00:55:41 -0300

    Se agrega framework
    - Este se agrega para crear un endpoint con la documentación de la API RESTful

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Rent Cars API documentation')
+    .setDescription('This is API for management rents of cars')
+    .setVersion('1.0')
+    .addTag('Users')
+    .addTag('Auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
