7a067683668a2cc28dabbf01c0452d99d77fb192
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-12-28 00:52:17 -0300

    Se modifica el modulo de autenticación
    - Esto se hace para corregir un error por el cual no leia bien las variables de entorno y no daba acceso a un usuario que inicio sesion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [],
     UsersModule,
+      useFactory: (config: ConfigService) => ({
+        dialect: config.get('database.dialect'),
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.user'),
+        password: config.get('database.password'),
+        database: config.get('database.name'),
+        models: [User],
     AuthModule,
   controllers: [AppController],
   providers: [AppService],
