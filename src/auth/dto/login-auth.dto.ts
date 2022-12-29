fd43dd1cbe2a51d82306eeec3d82439da3c15c3b
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-12-29 17:37:52 -0300

    Se agregan decoradores
    Estos se agregan para especificar ciertas propiedades en la documentacion de la API

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -1,9 +1,14 @@
-import { IsEmail, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 
 export class LoginAuthDto {
+  @ApiProperty()
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
+  @IsNotEmpty()
   @Length(4, 20)
   password: string;
 }
